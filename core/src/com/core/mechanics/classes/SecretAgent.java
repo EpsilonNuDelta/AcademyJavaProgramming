06155d67dabbf96e5016f72ed2857a7cfdcf78cb
420Memes <charlie.lyster@createthenext.com>
2016-06-28 11:26:13 -0700

    Fix

--- core/src/com/core/mechanics/classes/SecretAgent.java
+++ core/src/com/core/mechanics/classes/SecretAgent.java
@@ -2,9 +2,9 @@ package com.core.mechanics.classes;
 
 import com.core.mechanics.player.Player;
 
-public class Secretagent extends Player{
+public class SecretAgent extends Player{
 
-	protected Secretagent() {
+	protected SecretAgent() {
 		super("Secret Agent", 75, 20, 10, 100, 0);
 	}
 
