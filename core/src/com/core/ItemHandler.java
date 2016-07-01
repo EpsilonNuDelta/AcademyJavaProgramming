f033fe435b31e143af77e565adf6b72f4148e671
Alrynx <student@dhcp-x234-192.mobile.uci.edu>
2016-07-01 09:06:30 -0700

    Small sound fix

--- core/src/com/core/ItemHandler.java
+++ core/src/com/core/ItemHandler.java
@@ -14,18 +14,18 @@ public class ItemHandler{
 	private ArrayList<Weapons> w;
 	private ArrayList<Armour> a;
 	public final static String LASER_SOUND = "89489_mparsons99_laser1_converted.wav";
-	public final static String LASER_CANNON_SOUND = "Laser_Cannon-Mike_Koenig-797224747.mp3";
+	public final static String BULLET_SOUND = "150137_gregsmedia_gun-shot-sound-01_converted.wav";
 	public final static String SHOTGUN_SOUND = "Shotgun Sound.mp3";
 	public final static String MELEE_SOUND = "60009_qubodup_swosh-22.wav";
 
 	public ItemHandler(TiledMap map) {
 		w = new ArrayList<Weapons>();
 		a = new ArrayList<Armour>();
-		w.add(new RangedWeapon("BoltActionRifle",LASER_SOUND,20,10,5,map));
-		w.add(new RangedWeapon("AssaultRifle",LASER_SOUND,10,50,1,map));
+		w.add(new RangedWeapon("BoltActionRifle",BULLET_SOUND,20,10,5,map));
+		w.add(new RangedWeapon("AssaultRifle",BULLET_SOUND,10,50,1,map));
 		//w.add(new RangedWeapon("PumpShotgun",SHOTGUN_SOUND,17,10,3,map));
 		//w.add(new RangedWeapon("Pistol",10,20,2,map));
-		w.add(new RangedWeapon("Revolver",LASER_CANNON_SOUND,12,20,2,map));
+		w.add(new RangedWeapon("Revolver",BULLET_SOUND,12,20,2,map));
 		//w.add(new EnergyWeapons("PulseGun",15,30,2,map));
 		w.add(new EnergyWeapons("PulseRifle",LASER_SOUND,20,35,3,map));
 		w.add(new EnergyWeapons("ChargePistolDiverTram16by16",LASER_SOUND,10,35,2,map));
@@ -33,8 +33,8 @@ public class ItemHandler{
 		w.add(new MeleeWeapons("Baton", MELEE_SOUND,10, 2,map));
 		w.add(new MeleeWeapons("ShockBaton", MELEE_SOUND,15, 2,map));
 		w.add(new MeleeWeapons("EmergencyAxe", MELEE_SOUND,10, 2,map));
-		w.add(new RangedWeapon("Grey AR", LASER_SOUND,10, 2,5,map));
-		w.add(new RangedWeapon("grey pistol", LASER_SOUND,10, 2,5,map));
+		w.add(new RangedWeapon("Grey AR", BULLET_SOUND,10, 2,5,map));
+		w.add(new RangedWeapon("grey pistol", BULLET_SOUND,10, 2,5,map));
 		w.add(new MeleeWeapons("Chainsaw KatanaDiverTram16by16", MELEE_SOUND,12, 2,map));
 		w.add(new MeleeWeapons("Sword", MELEE_SOUND,15, 2,map));
 		a.add(new Armour("LightArmor", 15, map));
